2bfbc2f3ea523bb902ecbafb8542d2c8a5f83fb9
carolbatista <carolbatista2@gmail.com>
2021-07-27 21:06:42 -0300

    Usando transition para mostrar informações do vídeo. Exibindo vídeo utilizando o fancyBox

--- js/meuscript.js
+++ js/meuscript.js
@@ -21,14 +21,22 @@ $(document).ready(function() {
             var imagem;
             var arquivo;
             $.each(data.items, function(i ,item){
-
-                imagem = item.snippet.thumbnails.medium.url;
-                arquivo ='<li><img src="' + imagem + '"/></li>';  
+				console.log(data);
+                imagem    = item.snippet.thumbnails.medium.url;
+                titulo    = item.snippet.title;
+				publicado = formatarData(item.snippet.publishedAt);
+				videoId   = item.snippet.resourceId.videoId;
+                arquivo   ='<li class="principal"><a class="fancybox-media" href="https://www.youtube.com/watch?v=' + videoId +'"><div class="foto"><img src="' + imagem + '"/><div class="legenda"><h5>'+ titulo + '</h5><p>Data: '+ publicado +'</p></div></div></a></li>';
                 $('div#janela ul').append(arquivo);
             });
 
         }
         )
     }
+	function formatarData(data){
+
+		return data.substr(8,2) + '/'+ data.substr(5,2) + '/'+ data.substr(0,4);
+
+	}
 
 });
